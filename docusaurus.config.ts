1128f4c959cfb2db69805f61e0f96b77570a9be9
Muhib Sami <153531960+Muhibsami16@users.noreply.github.com>
2025-12-25 21:15:30 +0500

    url change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-ros2-education-site.github.io',
+  url: 'https://ai-native-development-sandy.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
